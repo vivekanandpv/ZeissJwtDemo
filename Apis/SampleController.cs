9fd11f3e0dd32fccdef0841d105c264c2df40f2e
Vivekanand P V <vivekanandpv@outlook.com>
2024-02-23 10:17:51 +0530

    Add project files.

---
+++ Apis/SampleController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ZeissJwtDemo.Apis
+{
+    [Route("api/v1/sample")]
+    [ApiController]
+    public class SampleController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Greet()
+        {
+            return Ok(await Task.FromResult("Good morning"));
+        }
+    }
+}
